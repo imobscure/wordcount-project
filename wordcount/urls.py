e0632713b9db229660b313f41b67b0543a7f2f37
Prakhar Pratap Singh <77916096+imobscure@users.noreply.github.com>
2023-04-23 22:18:44 +0530

    Update urls.py

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -18,6 +18,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
-    path('count/', views.count, name = 'count'),
-    path('about/', views.about, name = 'about'),
+    path('count', views.count, name = 'count'),
+    path('about', views.about, name = 'about'),
 ]
